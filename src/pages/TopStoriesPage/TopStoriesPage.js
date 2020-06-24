cb2d8826a5ba4fc549fe97b3f95957adcb777b89
Sava Paroski <sava.paroski021@gmail.com>
2020-06-24 13:37:18 +0200

    Added state,effect hooks, and fetched posts from api.

--- src/pages/TopStoriesPage/TopStoriesPage.js
+++ src/pages/TopStoriesPage/TopStoriesPage.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Post from '../../components/Post'
 import axios from '../../utility/axios'
 
 export default function TopStoriesPage() {
+  let [postIds, setPostIds] = useState([]);
+  useEffect(() => {
+    axios.get('/topstories.json?print=pretty').then(res => {
+      setPostIds(res.data);
+    }).catch(error => {
+
+    });
+  }, []);
+
   return (
     <div className="container p-0">
       <Post />
