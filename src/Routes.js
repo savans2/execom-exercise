be81aa7ca60c4bf4434d06200d7bdafc5c0f4ccf
Sava Paroski <sava.paroski021@gmail.com>
2020-06-24 13:16:19 +0200

    Added page (TopStories).

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+import TopStoriesPage from './pages/TopStoriesPage/TopStoriesPage'
 
 export default function Routes() {
   return (
     <React.Fragment>
       <Switch>
-        {/* <Route path="/" exact component={Component} /> */}
+        <Route path="/" exact component={TopStoriesPage} />
       </Switch>
     </React.Fragment>
   )
