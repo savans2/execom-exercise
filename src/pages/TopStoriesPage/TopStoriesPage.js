6c8249a214458a64600d6ed562469f7758e4fed7
Sava Paroski <sava.paroski021@gmail.com>
2020-06-24 16:02:52 +0200

    Added basic list render.

--- src/pages/TopStoriesPage/TopStoriesPage.js
+++ src/pages/TopStoriesPage/TopStoriesPage.js
@@ -5,7 +5,7 @@ import { chunkArray } from '../../utility/helperFunctions'
 
 export default function TopStoriesPage() {
   // Contains ids of posts in chunks of 30.
-  let [postIds, setPostIds] = useState([]);
+  const [postIds, setPostIds] = useState([[]]);
 
   useEffect(() => {
     axios.get('/topstories.json?print=pretty').then(res => {
@@ -15,9 +15,16 @@ export default function TopStoriesPage() {
     });
   }, []);
 
+  const listPosts = postIds[0].map((id, index) =>
+    <Post key={id} data={{
+      postID: id,
+      index: index + 1
+    }} />
+  );
+
   return (
     <div className="container p-0">
-      <Post />
+      {listPosts}
     </div>
   )
 }
