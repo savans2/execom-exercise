dff0c1b7bf379b904e7784defe6bb3e0d0491586
Sava Paroski <sava.paroski021@gmail.com>
2020-06-24 17:03:29 +0200

    Filled layout with basic info.

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,22 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from '../utility/axios'
 
-export default function Post() {
+export default function Post(props) {
+  const [postData, setPostData] = useState({});
+
+  useEffect(() => {
+    axios.get(`/item/${props.data.postID}.json?print=pretty`)
+      .then(res => {
+        setPostData(res.data);
+      });
+  }, []);
+
   return (
     <div className="bg-light pb-2">
       <div className="d-flex flex-wrap align-items-center">
-        <span className="mx-2">1.</span>
+        <span className="mx-2">{props.data.index}</span>
         <span>^</span>
-        <h6 className="my-0 mx-2">Title of the post goes here</h6>
+        <h6 className="my-0 mx-2">{postData.title}</h6>
         <p className="my-0">(www.example.com)</p>
       </div>
       <div className="d-flex flex-wrap">
-        <p className="my-0 mx-1">100 points by</p>
-        <a href="/#" className="my-0 mx-1">User</a>
-        <a href="/#" className="my-0 mx-1">1 minutes ago</a>
+        <p className="my-0 mx-1">{postData.score} points</p>
+        <a href="/#" className="my-0 mx-1">{postData.by}</a>
+        <a href="/#" className="my-0 mx-1">{postData.time}</a>
         <a href="/#" className="my-0 mx-1">hide</a>
-        <a href="/#" className="my-0 mx-1">past</a>
-        <a href="/#" className="my-0 mx-1">web</a>
-        <a href="/#" className="my-0 mx-1">discuss</a>
+        <a href="/#" className="my-0 mx-1">comment</a>
       </div>
     </div>
   )
