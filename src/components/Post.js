a54affb06be65fc573c9cb6061ca0b46100a1868
Sava Paroski <sava.paroski021@gmail.com>
2020-06-24 12:16:19 +0200

    Created dummy layout for post component.

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,7 +2,22 @@ import React from 'react'
 
 export default function Post() {
   return (
-    <div>
+    <div className="bg-light">
+      <div className="d-flex flex-wrap align-items-center">
+        <span className="mx-2">1.</span>
+        <span>^</span>
+        <h6 className="my-0 mx-2">Title of the post goes here</h6>
+        <p className="my-0">(www.example.com)</p>
+      </div>
+      <div className="d-flex flex-wrap">
+        <p className="my-0 mx-1">100 points by</p>
+        <a href="/#" className="my-0 mx-1">User</a>
+        <a href="/#" className="my-0 mx-1">1 minutes ago</a>
+        <a href="/#" className="my-0 mx-1">hide</a>
+        <a href="/#" className="my-0 mx-1">past</a>
+        <a href="/#" className="my-0 mx-1">web</a>
+        <a href="/#" className="my-0 mx-1">discuss</a>
+      </div>
     </div>
   )
 }
