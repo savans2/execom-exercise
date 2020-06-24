557a72fcb8b6adf82b1a60176a5d489c7ebd209c
Sava Paroski <sava.paroski021@gmail.com>
2020-06-24 13:21:31 +0200

    Created layout for top stories page.

--- src/pages/TopStoriesPage/TopStoriesPage.js
+++ src/pages/TopStoriesPage/TopStoriesPage.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Post from '../../components/Post'
 
 export default function TopStoriesPage() {
   return (
-    <div>
-
+    <div className="container p-0">
+      <Post />
     </div>
   )
 }
