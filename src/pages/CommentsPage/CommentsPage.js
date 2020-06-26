8376271b09bd137db99021167cc5d9cea9d2862d
Sava Paroski <sava.paroski021@gmail.com>
2020-06-26 14:35:37 +0200

    Added state to component, and api call if kids are not set.

--- src/pages/CommentsPage/CommentsPage.js
+++ src/pages/CommentsPage/CommentsPage.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Comment from '../../components/Comment';
-export default function CommentsPage() {
+import axios from '../../utility/axios';
+
+export default function CommentsPage(props) {
+  const [postID, setPostID] = useState(props.match.params.postID);
+  const [kids, setKids] = useState(
+    props.location.data !== undefined ? props.location.data.kids : null
+  );
+
+  useEffect(() => {
+    if (kids === null) {
+      axios.get(`/item/${postID}.json?print=pretty`).then(res => {
+        setKids(res.data.kids)
+      });
+    }
+  });
+
   return (
-    <div>
-      <Comment />
+    <div className="container">
+      <Comment data={{ kids }} />
     </div>
   )
 }
