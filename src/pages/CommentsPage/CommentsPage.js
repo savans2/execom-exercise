75eb8e2617616f4f036689e12e43c4cda07e6fb3
Sava Paroski <sava.paroski021@gmail.com>
2020-06-26 17:12:15 +0200

    Displayed comment, changed default value for kids.

--- src/pages/CommentsPage/CommentsPage.js
+++ src/pages/CommentsPage/CommentsPage.js
@@ -5,20 +5,24 @@ import axios from '../../utility/axios';
 export default function CommentsPage(props) {
   const [postID, setPostID] = useState(props.match.params.postID);
   const [kids, setKids] = useState(
-    props.location.data !== undefined ? props.location.data.kids : null
+    props.location.data !== undefined ? props.location.data.kids : []
   );
 
   useEffect(() => {
-    if (kids === null) {
+    if (kids.length === 0) {
       axios.get(`/item/${postID}.json?print=pretty`).then(res => {
-        setKids(res.data.kids)
+        setKids(res.data.kids);
       });
     }
   });
 
+  const listComments = kids.map((commentID, index) => {
+    return <Comment data={{ commentID }} key={kids[index]} />
+  });
+
   return (
     <div className="container">
-      <Comment data={{ kids }} />
+      {listComments}
     </div>
   )
 }
