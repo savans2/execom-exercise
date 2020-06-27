930b551914bd9a7a247847d29f5dfc72fb827269
Sava Paroski <sava.paroski021@gmail.com>
2020-06-27 16:30:43 +0200

    Added basic layout.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <div>
-
+    <div className="container bg-primary p-0 d-flex flex-wrap">
+      <div>
+        <a href="/#" className="text-white mx-1">[Y]</a>
+        <a href="/#" className="text-white mx-1">Hacker News</a>
+      </div>
+      <a href="/#" className="text-white mx-1">new</a>
+      <a href="/#" className="text-white mx-1">past</a>
+      <a href="/#" className="text-white mx-1">comments</a>
+      <a href="/#" className="text-white mx-1">ask</a>
+      <a href="/#" className="text-white mx-1">show</a>
+      <a href="/#" className="text-white mx-1">jobs</a>
+      <a href="/#" className="text-white mx-1">submit</a>
+      <a href="/#" className="text-white mr-1 ml-auto">login</a>
     </div>
   )
 }
