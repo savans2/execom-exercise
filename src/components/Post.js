d3da8c1f86ceb0ecbd183cb790da4551f9320eb7
Sava Paroski <sava.paroski021@gmail.com>
2020-06-27 17:37:20 +0200

    Fixed layout and its responsivness.

--- src/components/Post.js
+++ src/components/Post.js
@@ -16,14 +16,18 @@ export default function Post(props) {
   }, []);
 
   return (
-    <div className="bg-light pb-2">
-      <div className="d-flex flex-wrap align-items-center">
-        <span className="mx-2">{props.data.index}</span>
-        <span>▲</span>
-        <a href={postData.url} target="_blank" className="h6 my-0 mx-2 text-dark">{postData.title}</a>
-        <a href="/#" className="my-0 mx-2" style={{ fontSize: '14px' }}>{`${hostname}`}</a>
+    <div className="bg-light py-2">
+      <div className="d-flex">
+        <div>
+          <span className="mx-2">{props.data.index}</span>
+          <span>▲</span>
+        </div>
+        <a href={postData.url} target="_blank" className="h6 my-0 mx-2 text-dark">
+          {postData.title}
+          <a href="/#" className="my-0 mx-2" style={{ fontSize: '14px' }}>{`${hostname}`}</a>
+        </a>
       </div>
-      <div className="d-flex flex-wrap ml-5" style={{ fontSize: '14px' }}>
+      <div className="d-flex flex-wrap" style={{ fontSize: '14px', marginLeft: '42px' }}>
         <p className="my-0 mx-1">{postData.score} points</p>
         <a href="/#" className="my-0 mx-1">{postData.by}</a>
         <a href="/#" className="my-0 mx-1">{getRelativeTime(postData.time * 1000)}</a>
