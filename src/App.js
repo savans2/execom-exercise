99ff576b01a0013e30dfc1908000da3aede903ae
Sava Paroski <sava.paroski021@gmail.com>
2020-06-27 16:30:20 +0200

    Added navbar.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Routes from './Routes'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes />
     </div>
   );
