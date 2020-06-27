3dd211781956e8c8d557ce573b8d09766774f656
Sava Paroski <sava.paroski021@gmail.com>
2020-06-27 23:38:58 +0200

    Added rel=noopener noreferrer.

--- src/components/Post.js
+++ src/components/Post.js
@@ -28,7 +28,7 @@ export default function Post(props) {
           <span className="mx-2">{props.data.index}</span>
           <span>▲</span>
         </div>
-        <a href={postData.url} target="_blank" className="h6 my-0 mx-2 text-dark">
+        <a href={postData.url} target="_blank" rel="noopener noreferrer" className="h6 my-0 mx-2 text-dark">
           {postData.title}
           <a href="/#" className="my-0 mx-2" style={{ fontSize: '14px' }}>{`${hostname}`}</a>
         </a>
