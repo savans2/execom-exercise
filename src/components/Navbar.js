8df32a434698aba74ae3ca3077a1b997bc4c9db5
Sava Paroski <sava.paroski021@gmail.com>
2020-06-28 12:24:41 +0200

    Replaced anchor tags for links.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,8 @@ export default function Navbar() {
   return (
     <div className="container bg-primary p-0 d-flex flex-wrap">
       <div>
-        <a href="/#" className="text-white mx-1">[Y]</a>
-        <a href="/#" className="text-white mx-1">Hacker News</a>
+        <Link to="/" className="text-white mx-1">[Y]</Link>
+        <Link to="/" className="text-white mx-1">Hacker News</Link>
       </div>
       <a href="/#" className="text-white mx-1">new</a>
       <a href="/#" className="text-white mx-1">past</a>
