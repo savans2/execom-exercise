fde2022176be1f7edc527bd0e5d29204156b347c
Sava Paroski <sava.paroski021@gmail.com>
2020-07-08 12:55:54 +0200

    Corrected relative paths.

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import axios from '../utility/axios'
-import { getRelativeTime } from '../utility/helperFunctions';
+import axios from '../../utility/axios'
+import { getRelativeTime } from '../../utility/helperFunctions';
 import { Link } from 'react-router-dom';
 
 export default function Post(props) {
