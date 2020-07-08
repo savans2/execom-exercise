d111d2bab75a22ee8e886be0908ac5220276ed10
Sava Paroski <sava.paroski021@gmail.com>
2020-07-08 19:30:14 +0200

    Created loader with styled components.

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from 'react'
 import axios from '../../utility/axios'
 import { getRelativeTime } from '../../utility/helperFunctions';
 import { Link } from 'react-router-dom';
+import {
+  Container,
+  Title,
+  Item
+} from './style.js';
 
 export default function Post(props) {
   const [postData, setPostData] = useState({});
@@ -26,57 +31,51 @@ export default function Post(props) {
     }
   }
 
-  const loadingLayout = <React.Fragment>
-    <div>
-      <span className="mx-1 px-2">||</span>
-      <span>||</span>
+  const loadingLayout =
+    <div className={`bg-light py-2 d-flex`}>
+      <Container className="d-flex flex-wrap">
+        <Title randomWidth={Math.floor(Math.random() * 15)} />
+        <Item className="mx-2" randomWidth={Math.floor(Math.random() * 30)} />
+        <div className="d-flex flex-wrap ml-5">
+          <Item className="m-1" randomWidth={Math.floor(Math.random() * 30)} />
+          <Item className="m-1" randomWidth={Math.floor(Math.random() * 50)} />
+          <Item className="m-1" randomWidth={Math.floor(Math.random() * 30)} />
+          <Item className="m-1" randomWidth={Math.floor(Math.random() * 40)} />
+        </div>
+      </Container>
     </div>
-    <div>
-      <span target="_blank" rel="noopener noreferrer" className="h6 my-0 ml-2 text-dark">
-        |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-      </span>
-      <span href="/#" className="my-0 mx-2" style={{ fontSize: '14px' }}>||||||||||||||</span>
-      <div className="d-flex flex-wrap mx-1" style={{ fontSize: '14px' }}>
-        <p className="my-0 mx-1">||||||||||||</p>
-        <span href="/#" className="my-0 mx-1">||||||||||||||</span>
-        <span href="/#" className="my-0 mx-1">||||||||||</span>
-        <span className="my-0 mx-1">
-          |||||||||||||
-      </span>
-      </div>
-    </div>
-  </React.Fragment>
 
-  const loadedLayout = <React.Fragment>
-    <div>
-      <span className="mx-1">{props.data.index}</span>
-      <span>▲</span>
-    </div>
-    <div>
-      <a href={postData.url} target="_blank" rel="noopener noreferrer" className="h6 my-0 ml-2 text-dark">
-        {postData.title}
-      </a>
-      <a href="/#" className="my-0 mx-2" style={{ fontSize: '14px' }}> {`${hostname}`}</a>
-      <div className="d-flex flex-wrap mx-1" style={{ fontSize: '14px' }}>
-        <p className="my-0 mx-1">{postData.score} points</p>
-        <a href="/#" className="my-0 mx-1">{postData.by}</a>
-        <span href="/#" className="my-0 mx-1">{getRelativeTime(postData.time * 1000)}</span>
-        <Link
-          to={{
-            pathname: `/comments/${postData.id}`,
-            data: postData
-          }}
-          className="my-0 mx-1"
-        >
-          {getCommentNumber()}
-        </Link>
+  const loadedLayout =
+    <div className="bg-light py-2 d-flex">
+      <div>
+        <span className="mx-1">{props.data.index}</span>
+        <span>▲</span>
+      </div>
+      <div>
+        <a href={postData.url} target="_blank" rel="noopener noreferrer" className="h6 my-0 ml-2 text-dark">
+          {postData.title}
+        </a>
+        <a href="/#" className="my-0 mx-2" style={{ fontSize: '14px' }}> {`${hostname}`}</a>
+        <div className="d-flex flex-wrap mx-1" style={{ fontSize: '14px' }}>
+          <p className="my-0 mx-1">{postData.score} points</p>
+          <a href="/#" className="my-0 mx-1">{postData.by}</a>
+          <span href="/#" className="my-0 mx-1">{getRelativeTime(postData.time * 1000)}</span>
+          <Link
+            to={{
+              pathname: `/comments/${postData.id}`,
+              data: postData
+            }}
+            className="my-0 mx-1"
+          >
+            {getCommentNumber()}
+          </Link>
+        </div>
       </div>
     </div>
-  </React.Fragment>
 
   return (
-    <div className="bg-light py-2 d-flex">
+    <React.Fragment>
       {loading ? loadingLayout : loadedLayout}
-    </div >
+    </React.Fragment >
   )
 }
