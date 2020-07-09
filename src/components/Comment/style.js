0847a51bdee7355963b42a95185a2a55f7beec2a
Sava Paroski <sava.paroski021@gmail.com>
2020-07-09 12:55:21 +0200

    Created loader styles with styled components.

--- src/components/Comment/style.js
+++ src/components/Comment/style.js
@@ -1,1 +1,27 @@
 import styled, { keyframes } from 'styled-components';
+
+const changeColor = keyframes`
+  0% {
+    background-color: #9c9c9c;
+  }
+  50% {
+    background-color: #0275d8;
+  }
+  100% {
+    background-color: #9c9c9c;
+  }
+`;
+
+export const Title = styled.div`
+  height: 21px;
+  width: ${props => props.randomWidth + 150 + 'px'};
+  background-color: #333333;
+  animation: ${changeColor} 4s infinite;
+`;
+
+export const Paragraph = styled.p`
+  height: 21px;
+  width: ${props => props.randomWidth + props.startWidth + '%'};
+  background-color: #333333;
+  animation: ${changeColor} 4s infinite;
+`;
